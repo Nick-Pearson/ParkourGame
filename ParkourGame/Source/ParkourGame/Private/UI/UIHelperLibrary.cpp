3037147fbfff35180afe520729ad0e59ede42b52
James Webb <james@ultra-horizon.com>
2017-12-25 00:46:45 +0000

    Get servers from python backend implemented

--- ParkourGame/Source/ParkourGame/Private/UI/UIHelperLibrary.cpp
+++ ParkourGame/Source/ParkourGame/Private/UI/UIHelperLibrary.cpp
@@ -1,4 +1,6 @@
 #include "UIHelperLibrary.h"
+#include "HttpModule.h"
+#include "IHttpResponse.h"
 
 FText UUIHelperLibrary::FormatToasterMessage(const FText& FormatString, const TArray<FText>& Parameters)
 {
@@ -11,4 +13,23 @@ FText UUIHelperLibrary::FormatToasterMessage(const FText& FormatString, const TA
 	}
 
 	return FText::Format(FTextFormat(FormatString), args);
+}
+
+void UUIHelperLibrary::GetServerList(FDNSResultReturnEvent event){
+    TSharedRef<IHttpRequest> HttpReq = FHttpModule::Get().CreateRequest();
+    
+    HttpReq->SetVerb("GET");
+    HttpReq->SetURL("https://api.parkour.ultra-horizon.com/servers");
+    
+    HttpReq->OnProcessRequestComplete().BindStatic(&UUIHelperLibrary::ProcessResponse, event);
+    
+    HttpReq->ProcessRequest();
+}
+
+void UUIHelperLibrary::ProcessResponse(FHttpRequestPtr ReqPtr, FHttpResponsePtr Response, bool Success, FDNSResultReturnEvent event){
+    if (!Success){
+        return;
+    }
+    TArray<FString> servers = {Response->GetContentAsString()};
+    event.Execute(servers);
 }
--- ParkourGame/Source/ParkourGame/Private/UI/UIHelperLibrary.h
+++ ParkourGame/Source/ParkourGame/Private/UI/UIHelperLibrary.h
@@ -2,7 +2,12 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
+#include "IHttpRequest.h"
 #include "UIHelperLibrary.generated.h"
+
+
+DECLARE_DYNAMIC_DELEGATE_OneParam( FDNSResultReturnEvent, const TArray<FString>&, IPAddresses );
+
 
 UCLASS()
 class UUIHelperLibrary : public UBlueprintFunctionLibrary
@@ -10,8 +15,9 @@ class UUIHelperLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-
 	UFUNCTION(BlueprintCallable, Category = "HUD|Toaster")
 	static FText FormatToasterMessage(const FText& FormatString, const TArray<FText>& Parameters);
-	
+    UFUNCTION(BlueprintCallable, Category = "HUD|Toaster")
+    static void GetServerList(FDNSResultReturnEvent event);
+    static void ProcessResponse(FHttpRequestPtr ReqPtr, FHttpResponsePtr Response, bool Success, FDNSResultReturnEvent event);
 };
