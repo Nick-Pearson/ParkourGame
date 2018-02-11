6b283161219ab56bd861e73ae1d8a1aaee3c6971
Nick Pearson <nicholas.pearson4@btinternet.com>
2018-02-11 19:28:09 +0000

    Missing build cs file

--- ParkourGame/Source/ParkourGame/ParkourGame.Build.cs
+++ ParkourGame/Source/ParkourGame/ParkourGame.Build.cs
@@ -9,6 +9,6 @@ public class ParkourGame : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
-        PrivateDependencyModuleNames.AddRange(new string[] { "HTTP" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "HTTP", "LowEntryJson" });
 	}
 }
