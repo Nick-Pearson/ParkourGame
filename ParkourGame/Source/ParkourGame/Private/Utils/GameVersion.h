cad7003dbec29878baa32635f13783ffd1e94061
James Webb <james@ultra-horizon.com>
2018-04-17 16:11:02 +0100

    Removed tackle after slide

--- ParkourGame/Source/ParkourGame/Private/Utils/GameVersion.h
+++ ParkourGame/Source/ParkourGame/Private/Utils/GameVersion.h
@@ -5,8 +5,8 @@
 #include "GameVersion.generated.h"
 
 #define GAME_VERSION_MAJOR 0
-#define GAME_VERSION_MINOR 4
-#define GAME_VERSION_PATCH 2
+#define GAME_VERSION_MINOR 5
+#define GAME_VERSION_PATCH 0
 
 USTRUCT(BlueprintType)
 struct FGameVersion
